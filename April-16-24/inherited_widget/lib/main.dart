0887cbe929161c640504aa6f7c7b3042bea38387
VaibhavPagare25 <vaibhavpagare1234@gmail.com>
2024-04-17 07:44:10 +0530

    Tried Inherited Widget

--- April-16-24/inherited_widget/lib/main.dart
+++ April-16-24/inherited_widget/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:inherited_widget/ephemeral_state.dart';
+import 'package:inherited_widget/implementation.dart';
+import 'package:inherited_widget/inherited_widget.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +12,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: EphemeralTrial(),
+    return const SharedData(
+      data: 50,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Implementation(),
+      ),
     );
   }
 }
