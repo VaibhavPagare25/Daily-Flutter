b0c7380e760893940352c61e3fdaa21cd005e3e9
VaibhavPagare25 <vaibhavpagare1234@gmail.com>
2024-04-26 19:02:14 +0530

    Just practive

--- April-16-24/inherited_widget/lib/inherited_widget.dart
+++ April-16-24/inherited_widget/lib/inherited_widget.dart
@@ -13,3 +13,17 @@ class SharedData extends InheritedWidget {
     return data != oldWidget.data;
   }
 }
+
+
+// class ModelData extends InheritedWidget {
+//   int data;
+//   ModelData({super.key, required this.data,required super.child});
+
+//   static ModelData of(BuildContext context){
+//     return context.dependOnInheritedWidgetOfExactType<ModelData>()!;
+//   }
+
+//   bool updateShouldNotify(ModelData oldWidget) {
+//     return data != oldWidget.data;
+//   }
+// }
