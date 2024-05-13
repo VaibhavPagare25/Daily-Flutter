3e592ea6a59446bd906546f5bca812b63eb03580
VaibhavPagare25 <vaibhavpagare1234@gmail.com>
2024-05-13 12:15:06 +0530

    Some changes

--- May-12-24/api_binding/lib/main.dart
+++ May-12-24/api_binding/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:api_binding/api_demo.dart';
 import 'package:api_binding/dummy.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -7,8 +8,8 @@ import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
-  var myData = secondMap();
-  print(myData['data']);
+  // var myData = secondMap();
+  // print(myData['data']);
 }
 
 class MyApp extends StatelessWidget {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ApiBindingDemo(),
+      home: ApiDemo(),
     );
   }
 }
@@ -31,6 +32,7 @@ class ApiBindingDemo extends StatefulWidget {
 }
 
 class _ApiBindingDemoState extends State {
+  List<dynamic> list = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,18 +56,28 @@ class _ApiBindingDemoState extends State {
     Uri url = Uri.parse("https://dummy.restapiexample.com/api/v1/employees");
 
     http.Response response = await http.get(url);
-    //log(jsonDecode(response.body));
+    log(response.body);
 
-    var jsonData = jsonDecode(response.body);
+    // print("In print1: ${response.body}");
 
-    log(jsonData['status']);
-    log(jsonData['data'][0]['employee_name']);
-    //log(jsonData['data']);
-    var list = await jsonData['data'];
+    var jsonData = json.decode(response.body);
+
+    //log(jsonData['status']);
+    //log(jsonData['data'][0]['employee_name']);
+
+    print("In print2: ${jsonData['data']}");
+    log(response.body);
+    // log(jsonData['data']);
+
+    //var list = await jsonData['data'];
     // print(list);
     // log(list);
     // for (var item in jsonData['data']) {
     //   log(item['employee_name']);
     // }
+
+    setState(() {
+      list = jsonData['data'];
+    });
   }
 }
